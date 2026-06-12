bb6a2702c6574d728761e778c52669691e16805c
SebastienFeser <sedukafe@gmail.com>
2026-06-12 17:01:49 +0200

    added hint about konami code and log in console

---
+++ js/easter-egg.js
@@ -0,0 +1,17 @@
+/**
+ * Console easter egg: a friendly message for curious devs who open DevTools.
+ */
+console.log(`%c
+   ____  _____
+  / ___||  ___|
+  \\___ \\| |_
+   ___) |  _|
+  |____/|_|
+
+  Hey there, curious dev 👋
+  Digging through the code? Nice instinct.
+  If you're looking for someone who does the same
+  with game engines: sebastien.feser@gmail.com
+
+  P.S. Old-school gamers know the code. 🎮
+`, 'color:#5a8dee; font-family:monospace; font-size:13px;');
